80cf52edcfdb3a0ff6853c432266a1bebbdaaae1
Alexey Razumovskiy <razumyanchik@mail.ru>
2021-08-26 20:58:45 +0300

    Number of rows of aliens

--- AlienInvasion/alien_invasion.py
+++ AlienInvasion/alien_invasion.py
@@ -106,24 +106,39 @@ class AlienInvasion:
         # Отображение последнего прорисованнoго экрана
         pygame.display.flip()
 
+    def _create_alien(self, alien_number, row_number):
+        """ Создание очередного прищшельца и размещение его в очередном ряду"""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)          # Добавляем его в группу пришельцев
+
     def _create_fleet(self):
         """ Создаем флот пришельцеы"""   
         
         # Создание одного абстрактного пришельца. 
-        # Он нужен для расчетов ширины, чтоб не обращаться все время к аттрибуту rect         
+        # Он нужен для расчетов ширины, чтоб не обращаться все время к атрибуту rect         
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size             # Извлекаем размерения одного корабля для расчетов
         # Посчитаем сколько их уместится в ряду
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x = available_space_x // (2 * alien_width)
 
-        # Создадим первый ряд
-        for alien_number in range(number_aliens_x):
-            # Создание очередного корабля и размещение его в ряду
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)          # Добавляем его в группу пришельцев
+        # ... и подсчитаем сколько мы можем уместить рядов на экране
+        ship_height = self.ship.rect.height
+        # ОСтавим место для своего корабля  - его высоту плюс троекратную высоту вражеского корабля
+        available_space_y = (self.settings.screen_height - (3 * alien_height) - ship_height)
+
+        # определяем количество рядов
+        number_rows = available_space_y // (2 * alien_height)
+
+        # Создадим флот ряд за рядом
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
 
         
         
